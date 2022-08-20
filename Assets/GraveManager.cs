1513d618a3dbaf8c9dd1f7b7dfcf44bc5fa707a9
WhySoup <svenjanssen12321@gmail.com>
2022-08-20 23:53:28 +0200

    Added some modifiers for zombies, no actual data tho

--- Assets/GraveManager.cs
+++ Assets/GraveManager.cs
@@ -6,10 +6,33 @@ public class GraveManager : MonoBehaviour
 {
     [SerializeField] private Transform Zombie;
 
+    private int BaseValue = 100;
+    private float GravestoneModifier = 0.25f;
+    private float LightModifier = 0.5f;
+    private float EnvironmentModifier = 0.1f;
+
+    private List<Transform> EnvironmentObjects = new List<Transform>();
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         StartCoroutine(_SpawnZombie());
+        StartCoroutine(_IncomeLoop());
+    }
+
+    private void Update()
+    {
+        //Raycast above grave to see what tier gravestone is there (limited range)
+    }
+
+    private IEnumerator _IncomeLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(60);
+            //gain money
+            int money = (int)(BaseValue * GravestoneModifier * LightModifier * EnvironmentModifier);
+        }
     }
 
     private IEnumerator _SpawnZombie()
@@ -23,5 +46,15 @@ public class GraveManager : MonoBehaviour
             Zombie.localRotation = Quaternion.Lerp(Quaternion.Euler(Vector3.right * 180), Quaternion.Euler(Vector3.zero), progress);
             yield return null;
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
     }
 }
