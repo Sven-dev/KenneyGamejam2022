ca719980122c6392a264db0e77f039ce0078c9fc
WhySoup <svenjanssen12321@gmail.com>
2022-08-21 14:06:54 +0200

    Fixed a bug where carried items got upgraded when walking over a resource

--- Assets/RequirementLabel.cs
+++ Assets/RequirementLabel.cs
@@ -6,6 +6,10 @@ public class RequirementLabel : MonoBehaviour
 {
     private Vector3 DefaultPosition;
 
+    [SerializeField] private Collider Collider;
+    [SerializeReference] private SpriteRenderer RequirementBorder;
+    [SerializeReference] private SpriteRenderer RequirementLbl;
+
     private void Start()
     {
         DefaultPosition = transform.position;
@@ -18,7 +22,17 @@ public class RequirementLabel : MonoBehaviour
 
         if (Mathf.Abs(transform.position.y - DefaultPosition.y) > 0.1f)
         {
-            gameObject.SetActive(false);
+            Collider.enabled = false;
+
+            RequirementLbl.enabled = false;
+            RequirementBorder.enabled = false;
+        }
+        else
+        {
+            Collider.enabled = true;
+
+            RequirementLbl.enabled = true;
+            RequirementBorder.enabled = true;
         }
     }
 }
