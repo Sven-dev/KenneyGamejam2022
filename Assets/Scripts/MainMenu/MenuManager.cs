55444c53edc83f285887ceaa5e505d7026b11f54
Greenclonk <TheGreenclonk@gmail.com>
2022-08-21 14:53:25 +0100

    Fixed Fencing

--- Assets/Scripts/MainMenu/MenuManager.cs
+++ Assets/Scripts/MainMenu/MenuManager.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    
-
     public void StartGame()
     {
         if (LevelManager.Instance != null)
@@ -18,4 +16,44 @@ public class MenuManager : MonoBehaviour
     {
         LevelManager.ExitGame();
     }
+
+    bool openHowTo = false;
+    public void SwitchHowTo()
+    {
+        if (!openHowTo)
+        {
+            if (openOption) SwitchOptions();
+            if (openCredits) SwitchCredits();
+
+        }
+
+
+    }
+
+    bool openOption = false;
+    public void SwitchOptions()
+    {
+        if (!openOption)
+        {
+            if (openHowTo) SwitchHowTo();
+            if (openCredits) SwitchCredits();
+
+        }
+
+
+    }
+
+    bool openCredits = false;
+    public void SwitchCredits()
+    {
+        if (!openCredits)
+        {
+            if (openHowTo) SwitchHowTo();
+            if (openOption) SwitchOptions();
+
+        }
+
+
+    }
+
 }
