fff790018676cccec68d1dea93ca2cd6cb51adba
Greenclonk <TheGreenclonk@gmail.com>
2022-08-21 09:32:27 +0100

    Morning Commit

---
+++ Assets/Scripts/UI Game/CraftButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftButton : MonoBehaviour
+{
+    [SerializeField]
+    int index = 0;
+    
+    public void ShowSelected()
+    {
+        if (CraftingManager.Instance)
+        {
+            CraftingManager.Instance.ShowSelected(index);
+        }
+    }
+    public void Selected()
+    {
+        if (CraftingManager.Instance)
+        {
+            CraftingManager.Instance.Craft(index);
+        }
+    }
+
+    
+
+}
